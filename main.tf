2ee49dcc900d8599023e0ed35fe83c1754cd1840
JoeStack <joern@tel2ip.net>
2018-11-29 12:36:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 //--------------------------------------------------------------------
+// Variables
+
+
+
+//--------------------------------------------------------------------
 // Modules
+module "ec2_instance" {
+  source  = "app.terraform.io/JoeStack/ec2-instance/aws"
+  version = "1.13.0"
+
+  ami = "ami-09351ab2e74aadeb2"
+  instance_type = "t2.small"
+  name = "foo"
+  subnet_id = "${module.vpc.private_subnets}"
+  vpc_security_group_ids = "${module.vpc.default_security_group_id}"
+}
+
 module "vpc" {
   source  = "app.terraform.io/JoeStack/vpc/aws"
   version = "1.46.0"
