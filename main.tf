ef67346ffab5d9d95f629984786f3e4e32541fc1
JoeStack <joern@tel2ip.net>
2018-11-29 12:10:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-
 //--------------------------------------------------------------------
 // Modules
 module "vpc" {
   source  = "app.terraform.io/JoeStack/vpc/aws"
   version = "1.46.0"
+
+  cidr = "10.0.0.0/8"
 }
